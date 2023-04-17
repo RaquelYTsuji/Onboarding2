4ef952942621aface3e7e8e90bbd95f25ddc4492
Raquel Tsuji <raqueltsuji@gmail.com>
2023-04-17 16:09:07 -0300

    Initial commit

---
+++ src/main/java/com/raquelcodelab/onboarding2/Onboarding2Application.java
@@ -0,0 +1,13 @@
+package com.raquelcodelab.onboarding2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onboarding2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onboarding2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/raquelcodelab/onboarding2/Onboarding2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raquelcodelab.onboarding2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onboarding2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
